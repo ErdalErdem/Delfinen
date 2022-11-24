6680c1f7e8bac0cf4cacf3af8acad34c207940d5
ErdalErdem <erda0028@stud.kea.dk>
2022-11-24 12:51:31 +0100

    Attributter og konstuctor er lavet

---
+++ src/main/java/Delfinen.java
@@ -0,0 +1,48 @@
+public class Delfinen {
+
+    private String navn;
+    private int alder;
+    private String køn;
+    private boolean typeMedlemskab;
+
+    public Delfinen(String navn, int alder, String køn, boolean typeMedlemskab) {
+        this.navn = navn;
+        this.alder = alder;
+        this.køn = køn;
+        this.typeMedlemskab = typeMedlemskab;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public String getKøn() {
+        return køn;
+    }
+
+    public void setKøn(String køn) {
+        this.køn = køn;
+    }
+    public boolean getTypeMedlemskab() {
+        return typeMedlemskab;
+    }
+    public void setTypeMedlemskab(boolean typeMedlemskab) {
+        this.typeMedlemskab = typeMedlemskab;
+    }
+
+
+    public String toString() {
+        return "Navn:" + " " + navn + "\nAlder:" + " " + alder + "\nKøn:" + køn + "\nType medlemskab:" + typeMedlemskab;
+    }
+}
