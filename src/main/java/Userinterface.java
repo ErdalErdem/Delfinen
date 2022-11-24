0ded5acaf112b68988f05fb59b6356f487bb7042
Kazo42 <kasi0172@stud.kea.dk>
2022-11-24 13:13:36 +0100

    ui

---
+++ src/main/java/Userinterface.java
@@ -0,0 +1,27 @@
+public class Userinterface {
+    Scanner scanner = new Scanner(System.in);
+
+    public void userChoice(int userChoice) {
+        userChoice = -1;
+        System.out.println("Velkommen til Delfinen! \n" +
+                "_____________");
+
+        while (userChoice != 9) {
+            System.out.println("""                  
+                    1. Opret medlemmer 
+                    2. Liste af medlemmere
+                    3. Redigering af medlemmere
+                    4. Slette medlemmere
+                    5. Tilføj medlemmere
+                    9. Afslut program
+                 
+                    
+                    """);
+
+            userChoice = scanner.nextInt();
+            scanner.nextLine(); // Håndtering af Scanner bug
+            userChoice(userChoice);
+
+
+        }
+    }
