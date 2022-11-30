262f518ba8bf8c5d8d461b60f0a74ed9f9123c6c
ErdalErdem <erda0028@stud.kea.dk>
2022-11-30 15:38:48 +0100

    Arbejdes på unit tests ikke færdigt endnu.

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+ /*   Database database = new Database();
+
+    // Hard koder nogen medlemmer, for ikke at skulle lave nye medlemmer hver gang.
+
+    @BeforeEach
+    void setMedlemmerOp() {
+        Medlem medlem1 = new Medlem("Bo", 20, "mand", Medlem.typeMedlem.aktiv);
+
+        Medlem medlem2 = new Medlem("Hans", 23, "mand", Medlem.typeMedlem.aktiv);
+
+        Medlem medlem3 = new Medlem("Bob", 25, "mand", Medlem.typeMedlem.aktiv);
+
+        database.getMedlemDB().addAll(Arrays.asList(medlem1, medlem2, medlem3));
+
+    }
+
+    @Test
+    void tilFoejMedlemmer() {
+
+        // Arrange
+        int forventetStørrelse = 5;
+        int forventedeStørrelseForPeter = 4;
+        String forventedeNavn = "Peter";
+
+        // Act
+        database.tilfoejMedlem("Peter", 20, "mand", Medlem.typeMedlem.aktiv);
+
+        return assertAll("Tester om medlem tilføjes",
+                () -> assertEquals(forventetStørrelse, database.getMedlemDB().size()),
+                () -> assertEquals(forventedeNavn, database.getMedlemDB().get(forventedeStørrelseForPeter).getNavn())
+*/
+
+                ;
+    //}
+}
