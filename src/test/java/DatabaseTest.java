e4e35463beaf7fef9c92e7368435267269e05330
ErdalErdem <erda0028@stud.kea.dk>
2022-12-02 12:54:44 +0100

    TilfoejMedlem er færdig, FindMedlem driller stadig SletMedlem driller stadig

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -7,35 +7,65 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
+    Database database;
 
-    @Test
-void tilFoejelseAfMedlemmer() {
-
-    // Arrange
-Database database = new Database();
-
-    // Act
-
-    database.tilfoejMedlem("Per", 20, "mand", Medlem.typeMedlem.aktiv);
-
-    // Assert
-    int forventetDBStørrelse = 1;
-    int faktiskStørrelse = database.getMedlemDB().size();
-    assertEquals(forventetDBStørrelse, faktiskStørrelse);
-}
-
-@Test
-    void findMedlemUdenNogetMatch() {
-
+    @BeforeEach
+    void set() {
         Database database = new Database();
 
+    }
 
-// ikke sikker på hvordan jeg skal lave unit test her
+    @Test
+    void tilFoejelseAfMedlemmer() {
+
+        // Arrange
+        Database database = new Database();
+
+        // Act
+
+        database.tilfoejMedlem("Per", 20, "mand", Medlem.typeMedlem.aktiv);
+
+        // Assert
+        int forventetDBStørrelse = 1;
+        int faktiskStørrelse = database.getMedlemDB().size();
+        assertEquals(forventetDBStørrelse, faktiskStørrelse);
+    }
+
+    @Test
+    void findMedlem() {
+
+        // Arrange
+        String navn = "Hans";
+        int alder = 25;
+        String køn = "Mand";
+        boolean aktivitet = true;
+
+        database.tilfoejMedlem(navn, alder, køn, Medlem.typeMedlem.aktiv); // Type medlem skal ændres til boolean når vi merger
+
+        // Act
+        Medlem resultat = database.findMedlem("Hans");
 
 
+        // Assert
+        assertEquals(resultat.getNavn(), navn);
+    }
+
+    @Test
+    void sletMedlem() {
+
+        // Arrange
+        int forventetStørrelse = 0;
+
+        database.tilfoejMedlem("Hans", 25, "Mand", Medlem.typeMedlem.aktiv);
+        // Act
+
+        Medlem faktiskStørrelse = database.findMedlem("Hans");
+
+        // Assert
+
+        assertEquals(forventetStørrelse, faktiskStørrelse);
 
 
-
-}
+    }
 
 }
